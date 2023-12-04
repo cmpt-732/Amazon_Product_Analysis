782cf09db6de18cbf37d1d49763ab87a533c37ff
Jarvis He <85099505+Ilikedietpepsi@users.noreply.github.com>
2023-12-04 13:03:22 -0800

    Add files via upload

--- Code/keywords.py
+++ Code/keywords.py
@@ -21,7 +21,7 @@ def get_keywords(x):
     r.extract_keywords_from_text(x)
     return r.get_ranked_phrases_with_scores()
 
-def main(inputs):
+def main():
             reviews_schema = types.StructType([
                 types.StructField('reviewerID', types.StringType()),
                 types.StructField('vote', types.StringType()),
@@ -35,18 +35,55 @@ def main(inputs):
                 types.StructField('reviewTime', types.StringType()),
             ])
 
-            reviews = spark.read.schema(reviews_schema).json("reviews/{file}".format(file=inputs)).select("asin","reviewText","overall").cache()
-            good_reviews = reviews.filter(reviews["overall"] >= 4.0)
-            bad_reviews = reviews.filter(reviews["overall"] <= 3.0)
+            office_reviews = spark.read.schema(reviews_schema).json("reviews/Office_Products_5.json.gz").select("asin","reviewText","overall").cache()
+            movies_reviews = spark.read.schema(reviews_schema).json("reviews/Movies_and_TV_5.json.gz").select("asin","reviewText","overall").cache()
+            clothing_reviews = spark.read.schema(reviews_schema).json("reviews/Clothing_Shoes_and_Jewelry_5.json.gz").select("asin","reviewText","overall").cache()
+
+            office_good_reviews = office_reviews.filter(office_reviews["overall"] >= 4.0)
+            office_bad_reviews = office_reviews.filter(office_reviews["overall"] <= 3.0)
             get_keywords_udf = udf(get_keywords, ArrayType(types.StructType([types.StructField("score", FloatType(), False),
     types.StructField("keywords", StringType(), False)])))
-            good_reviews = good_reviews.filter(good_reviews["reviewText"].isNotNull())
-            good_reviews = good_reviews.withColumn("key_words", get_keywords_udf(good_reviews["reviewText"]))
-            bad_reviews = bad_reviews.filter(bad_reviews["reviewText"].isNotNull())
-            bad_reviews = bad_reviews.withColumn("key_words", get_keywords_udf(bad_reviews["reviewText"]))
-            # good_reviews = good_reviews.select("asin", functions.concat_ws(", ", functions.col("key_words").cast("array<string>")).alias("key_words"))
-            good_reviews.toPandas()
-            bad_reviews.toPandas()
+            office_good_reviews = office_good_reviews.filter(office_good_reviews["reviewText"].isNotNull())
+            office_good_reviews = office_good_reviews.withColumn("key_words", get_keywords_udf(office_good_reviews["reviewText"]))
+            office_bad_reviews = office_bad_reviews.filter(office_bad_reviews["reviewText"].isNotNull())
+            office_bad_reviews = office_bad_reviews.withColumn("key_words", get_keywords_udf(office_bad_reviews["reviewText"]))
+            office_good_reviews.limit(500).toPandas()
+            office_bad_reviews.limit(500).toPandas()
+
+            office_good_reviews.show(20)
+
+            movies_good_reviews = movies_reviews.filter(movies_reviews["overall"] >= 4.0)
+            movies_bad_reviews = movies_reviews.filter(movies_reviews["overall"] <= 3.0)
+            get_keywords_udf = udf(get_keywords,
+                                   ArrayType(types.StructType([types.StructField("score", FloatType(), False),
+                                                               types.StructField("keywords", StringType(), False)])))
+            movies_good_reviews = movies_good_reviews.filter(movies_good_reviews["reviewText"].isNotNull())
+            movies_good_reviews = movies_good_reviews.withColumn("key_words",
+                                                                 get_keywords_udf(movies_good_reviews["reviewText"]))
+            movies_bad_reviews = movies_bad_reviews.filter(movies_bad_reviews["reviewText"].isNotNull())
+            movies_bad_reviews = movies_bad_reviews.withColumn("key_words",
+                                                               get_keywords_udf(movies_bad_reviews["reviewText"]))
+            movies_good_reviews.limit(500).toPandas()
+            movies_bad_reviews.limit(500).toPandas()
+
+            movies_good_reviews.show(20)
+
+            clothing_good_reviews = clothing_reviews.filter(clothing_reviews["overall"] >= 4.0)
+            clothing_bad_reviews = clothing_reviews.filter(clothing_reviews["overall"] <= 3.0)
+            get_keywords_udf = udf(get_keywords,
+                                   ArrayType(types.StructType([types.StructField("score", FloatType(), False),
+                                                               types.StructField("keywords", StringType(), False)])))
+            clothing_good_reviews = clothing_good_reviews.filter(clothing_good_reviews["reviewText"].isNotNull())
+            clothing_good_reviews = clothing_good_reviews.withColumn("key_words",
+                                                                 get_keywords_udf(clothing_good_reviews["reviewText"]))
+            clothing_bad_reviews = clothing_bad_reviews.filter(clothing_bad_reviews["reviewText"].isNotNull())
+            clothing_bad_reviews = clothing_bad_reviews.withColumn("key_words",
+                                                               get_keywords_udf(clothing_bad_reviews["reviewText"]))
+
+            clothing_good_reviews.limit(500).toPandas()
+            clothing_bad_reviews.limit(500).toPandas()
+
+            clothing_good_reviews.show(20)
 
 
 if __name__ == '__main__':
@@ -55,5 +92,5 @@ if __name__ == '__main__':
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
     assert sc.version >= '3.0'  # make sure we have Spark 3.0+
-    inputs = sys.argv[1]
-    main(inputs)
+
+    main()
